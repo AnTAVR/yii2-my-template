4c3de81eb35e385fc3e53e2f6a497442b72ab1ea
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 19:58:43 +0300

    -

---
+++ protected/modules/account/models/PasswordSaveForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\modules\account\models;
+
+use Yii;
+use yii\base\Model;
+
+class PasswordSaveForm extends Model
+{
+    public $password;
+    public $verifyPassword;
+    public $verifyCode;
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['password', 'verifyPassword'], 'required', 'on' => ['password-save']],
+            ['verifyPassword', 'compare', 'compareAttribute' => 'password', 'on' => ['password-save']],
+            ['verifyCode', 'captcha', 'on' => ['password-save']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'password' => Yii::t('app', 'Password'),
+            'verifyPassword' => Yii::t('app', 'Verification Password'),
+            'verifyCode' => Yii::t('app', 'Verification Code'),
+        ];
+    }
+
+    public function save()
+    {
+        return true;
+    }
+}
