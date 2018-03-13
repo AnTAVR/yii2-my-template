0e55659d0331980d5e8b47aef845f41b86a04e0e
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 21:12:30 +0300

    -

--- protected/modules/account/models/PasswordResetForm.php
+++ protected/modules/account/models/PasswordResetForm.php
@@ -8,19 +8,6 @@ class PasswordResetForm extends Model
 {
     use UserTrait;
 
-    /**
-     * @return array the validation rules.
-     */
-    public function rules()
-    {
-        return [
-            // username and password are both required
-            ['email', 'required', 'on' => ['password-reset']],
-            ['email', 'email', 'on' => ['password-reset']],
-            ['verifyCode', 'captcha', 'on' => ['password-reset']],
-        ];
-    }
-
     public function save()
     {
         return true;
