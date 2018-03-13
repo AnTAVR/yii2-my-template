e4405721c41ba5d413ad97d8ddb9b7435d1326c0
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 20:22:58 +0300

    -

---
+++ protected/modules/account/views/profile/password-edit.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this yii\web\View */
+
+/* @var $model app\modules\account\models\PasswordEditForm */
+
+use app\widgets\Captcha;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = Yii::t('app', 'Password Edit');
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="account-profile-password-edit">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php $form = ActiveForm::begin(['id' => 'password-edit-form']); /* @var $form yii\bootstrap\ActiveForm */ ?>
+
+            <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+
+</div>
