fd97e0945014103e812e815fc34245f6593124aa
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 19:54:52 +0300

    -

--- protected/modules/account/views/profile/signup.php
+++ protected/modules/account/views/profile/signup.php
@@ -20,6 +20,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
+            <?= $form->field($model, 'email')->textInput() ?>
+
             <?= $form->field($model, 'password')->passwordInput() ?>
 
             <?= $form->field($model, 'verifyPassword')->passwordInput() ?>
