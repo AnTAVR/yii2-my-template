1590297a188923d23411edf5566bde3025967eef
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 21:11:07 +0300

    -

--- protected/modules/account/models/SignupForm.php
+++ protected/modules/account/models/SignupForm.php
@@ -8,24 +8,6 @@ class SignupForm extends Model
 {
     use UserTrait;
 
-    /**
-     * @return array the validation rules.
-     */
-    public function rules()
-    {
-        return [
-            // username and password are both required
-            ['username', 'required', 'on' => ['signup']],
-            ['password', 'required', 'on' => ['signup']],
-            ['verifyPassword', 'required', 'on' => ['signup']],
-            ['verifyPassword', 'compare', 'compareAttribute' => 'password', 'on' => ['signup']],
-            ['email', 'required', 'on' => ['signup']],
-            ['email', 'email', 'on' => ['signup']],
-            ['email', 'unique', 'on' => ['signup']],
-            ['verifyCode', 'captcha', 'on' => ['signup']],
-        ];
-    }
-
     public function save()
     {
         return true;
