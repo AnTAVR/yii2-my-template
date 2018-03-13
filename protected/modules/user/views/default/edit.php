b4e962bbe795599b82d6959f3e5d6998bce38493
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 11:36:26 +0300

    -

--- protected/modules/user/views/default/edit.php
+++ protected/modules/user/views/default/edit.php
@@ -1,9 +1,14 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $context yii\web\Controller */
+$context = $this->context;
+?>
 <div class="user-default-edit">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+    <h1><?= $context->action->uniqueId ?></h1>
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
+        This is the view content for action "<?= $context->action->id ?>".
+        The action belongs to the controller "<?= get_class($context) ?>"
+        in the "<?= $context->module->id ?>" module.
     </p>
     <p>
         You may customize this page by editing the following file:<br>
