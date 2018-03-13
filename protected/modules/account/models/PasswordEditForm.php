5dd1e9c1a8189d359a2af1c5353526eafbf2ffa7
Anthony Lyappiev <admin@antavr.ru>
2018-03-13 21:12:04 +0300

    -

--- protected/modules/account/models/PasswordEditForm.php
+++ protected/modules/account/models/PasswordEditForm.php
@@ -8,19 +8,6 @@ class PasswordEditForm extends Model
 {
     use UserTrait;
 
-    /**
-     * @return array the validation rules.
-     */
-    public function rules()
-    {
-        return [
-            ['password', 'required', 'on' => ['password-edit']],
-            ['verifyPassword', 'required', 'on' => ['password-edit']],
-            ['verifyPassword', 'compare', 'compareAttribute' => 'password', 'on' => ['password-edit']],
-            ['verifyCode', 'captcha', 'on' => ['password-edit']],
-        ];
-    }
-
     public function save()
     {
         return true;
