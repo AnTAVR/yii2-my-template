14fd7309c374d78852f624e7bd49abeac755aedc
Anthony Lyappiev <admin@antavr.ru>
2018-03-14 20:47:09 +0300

    -

--- protected/modules/account/models/LoginForm.php
+++ protected/modules/account/models/LoginForm.php
@@ -3,20 +3,13 @@
 namespace app\modules\account\models;
 
 use Yii;
-use yii\base\Model;
 
 /**
  * LoginForm is the model behind the login form.
  *
- * @property User|null $user This property is read-only.
- *
  */
-class LoginForm extends Model
+class LoginForm extends User
 {
-    use UserTrait;
-
-    private $_user = false;
-
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
@@ -29,26 +22,10 @@ class LoginForm extends Model
                                           $params)
     {
         if (!$this->hasErrors()) {
-            $user = $this->getUser();
-
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+            if (!$this->validatePassword($this->getAttribute($attribute))) {
+                $this->addError($attribute, Yii::t('app', 'Incorrect username or password.'));
             }
         }
-    }
-
-    /**
-     * Finds user by [[username]]
-     *
-     * @return User|null
-     */
-    public function getUser()
-    {
-        if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
-        }
-
-        return $this->_user;
     }
 
     /**
@@ -58,7 +35,7 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+            return Yii::$app->user->login($this, $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
         return false;
     }
