503c26942a13400c3aa9a5d78cb2d95e4f4b27ec
Anthony Lyappiev <admin@antavr.ru>
2018-03-14 13:10:47 +0300

    -

--- protected/modules/account/views/profile/password-edit.php
+++ protected/modules/account/views/profile/password-edit.php
@@ -23,6 +23,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-6">
             <?php $form = ActiveForm::begin(['id' => 'password-edit-form']); /* @var $form yii\bootstrap\ActiveForm */ ?>
 
+            <?= $form->field($model, 'oldPassword')->passwordInput() ?>
+
             <?= $form->field($model, 'password')->passwordInput() ?>
 
             <?= $form->field($model, 'verifyPassword')->passwordInput() ?>
