55d16f78686238dcdc7b6093bbb53653a9a531ee
Anthony Lyappiev <admin@antavr.ru>
2018-03-14 19:36:06 +0300

    -

--- protected/modules/account/models/SignupForm.php
+++ protected/modules/account/models/SignupForm.php
@@ -2,7 +2,9 @@
 
 namespace app\modules\account\models;
 
+use Yii;
 use yii\base\Model;
+use app\modules\account\models\UserDd as User;
 
 class SignupForm extends Model
 {
@@ -10,6 +12,22 @@ class SignupForm extends Model
 
     public function save()
     {
-        return true;
+        if ($this->validate()) {
+            $user = new User();
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+            $user->save(false);
+
+//  the following three lines were added:
+//            $auth = Yii::$app->authManager;
+//            $authorRole = $auth->getRole('author');
+//            $auth->assign($authorRole, $user->getId());
+
+            return $user;
+        }
+
+        return null;
     }
 }
