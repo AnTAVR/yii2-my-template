a1070bec93b490b1e6581e23100467dfb44406db
Anthony Lyappiev <admin@antavr.ru>
2018-03-14 18:22:12 +0300

    -

---
+++ protected/modules/account/migrations/m000099_000000_create_user.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\db\Expression;
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `user`.
+ */
+class m000099_000000_create_user extends Migration
+{
+    public $tableName = '{{%user}}';
+
+    // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+    public $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        if ($this->db->driverName !== 'mysql') {
+            $this->tableOptions = null;
+        }
+
+        $this->createTable($this->tableName, [
+            'username' => $this->string()->notNull()->unique(),
+            'password' => $this->string()->notNull(),
+            'email' => $this->string()->notNull()->unique(),
+            'email_status' => $this->boolean()->notNull()->defaultValue(false),
+
+            'foto' => $this->string(),
+
+            'status' => $this->smallInteger()->notNull(),
+
+            'created_at' => $this->dateTime()->defaultValue(new Expression('NOW()')),
+
+            'session_at' => $this->dateTime(),
+            'session' => $this->string(),
+
+            'PRIMARY KEY ([[username]])',
+        ], $this->tableOptions);
+
+        $name = 'session';
+        $this->createIndex($name, $this->tableName, $name);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $name = 'published_at';
+        $this->dropIndex($name, $this->tableName);
+
+        $this->dropTable($this->tableName);
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
