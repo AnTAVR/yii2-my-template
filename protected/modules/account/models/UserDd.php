857144e231ed5c375345a1bb33cbb52d5efc5973
Anthony Lyappiev <admin@antavr.ru>
2018-03-14 19:27:30 +0300

    -

--- protected/modules/account/models/UserDd.php
+++ protected/modules/account/models/UserDd.php
@@ -63,6 +63,17 @@ class UserDd extends ActiveRecord implements IdentityInterface
     }
 
     /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+    /**
      * @return string current user auth key
      */
     public function getAuthKey()
@@ -72,12 +83,15 @@ class UserDd extends ActiveRecord implements IdentityInterface
 
     public function beforeSave($insert)
     {
-        if (parent::beforeSave($insert)) {
-            if ($this->isNewRecord) {
-                $this->auth_key = Yii::$app->security->generateRandomString();
-            }
-            return true;
+        $security = Yii::$app->security;
+        if ($this->isNewRecord) {
+            $this->auth_key = $security->generateRandomString();
         }
-        return false;
+
+        if (!empty($this->password)) {
+            $this->password = $security->generatePasswordHash($this->password);
+        }
+
+        return parent::beforeSave($insert);
     }
 }
