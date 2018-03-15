a3997a786bc48447e39b5f6fdf6071e6a9a432eb
Anthony Lyappiev <admin@antavr.ru>
2018-03-15 16:31:03 +0300

    -

--- protected/modules/account/models/PasswordEditForm.php
+++ protected/modules/account/models/PasswordEditForm.php
@@ -2,11 +2,77 @@
 
 namespace app\modules\account\models;
 
-use yii\base\Model;
+use Yii;
+use yii\helpers\ArrayHelper;
 
-class PasswordEditForm extends Model
+class PasswordEditForm extends User
 {
-    use UserTrait;
+    public $password;
+    public $verifyPassword;
+    public $oldPassword;
+    public $verifyCode;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        $params = Yii::$app->params;
+        $rules = [
+            ['verifyCode', 'captcha'],
+
+            ['password', 'required'],
+            ['password', 'string',
+                'max' => $params['password.max'],
+                'min' => $params['password.min']],
+
+            ['verifyPassword', 'required'],
+            ['verifyPassword', 'compare',
+                'compareAttribute' => 'password'],
+
+            ['oldPassword', 'required'],
+
+            ['oldPassword', 'validateOldPassword'],
+        ];
+        return ArrayHelper::merge(parent::rules(), $rules);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        $labels = [
+            'password' => Yii::t('app', 'Password'),
+            'oldPassword' => Yii::t('app', 'Old Password'),
+            'verifyPassword' => Yii::t('app', 'Verification Password'),
+            'verifyCode' => Yii::t('app', 'Verification Code'),
+        ];
+        return ArrayHelper::merge(parent::attributeLabels(), $labels);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeHints()
+    {
+        $hints = [
+            'password' => Yii::t('app', 'Set a complex password using uppercase and lowercase letters, numbers and special characters.'),
+        ];
+
+        return ArrayHelper::merge(parent::attributeHints(), $hints);
+    }
+
+    /**
+     * @param $attribute
+     * @param $params
+     * @return bool
+     */
+    public function validateOldPassword(/** @noinspection PhpUnusedParameterInspection */
+        $attribute, $params)
+    {
+        return false;
+    }
 
     /**
      * @return bool
