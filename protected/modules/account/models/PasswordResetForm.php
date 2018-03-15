9a0638035fe3dee602e1081b3385b7421fbcd049
Anthony Lyappiev <admin@antavr.ru>
2018-03-15 16:22:26 +0300

    -

--- protected/modules/account/models/PasswordResetForm.php
+++ protected/modules/account/models/PasswordResetForm.php
@@ -2,11 +2,53 @@
 
 namespace app\modules\account\models;
 
-use yii\base\Model;
+use Yii;
+use yii\helpers\ArrayHelper;
 
-class PasswordResetForm extends Model
+class PasswordResetForm extends User
 {
-    use UserTrait;
+    public $verifyCode;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        $params = Yii::$app->params;
+        $rules = [
+            ['verifyCode', 'captcha'],
+
+            ['email', 'required'],
+            ['email', 'string', 'max' => $params['email.max']],
+            ['email', 'email'],
+
+            ['email', 'exist'],
+        ];
+        return ArrayHelper::merge(parent::rules(), $rules);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        $labels = [
+            'verifyCode' => Yii::t('app', 'Verification Code'),
+        ];
+        return ArrayHelper::merge(parent::attributeLabels(), $labels);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeHints()
+    {
+        $hints = [
+            'email' => Yii::t('app', 'Enter E-Mail corresponding to the account, it will be sent an email with instructions.'),
+        ];
+
+        return ArrayHelper::merge(parent::attributeHints(), $hints);
+    }
 
     /**
      * @return bool
