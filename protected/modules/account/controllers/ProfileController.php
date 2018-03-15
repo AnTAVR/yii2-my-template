b7e15257e73178766d4a6bb7867308ef441e62d9
Anthony Lyappiev <admin@antavr.ru>
2018-03-15 15:00:08 +0300

    -

--- protected/modules/account/controllers/ProfileController.php
+++ protected/modules/account/controllers/ProfileController.php
@@ -2,44 +2,28 @@
 
 namespace app\modules\account\controllers;
 
-use app\modules\account\models\LoginForm;
 use app\modules\account\models\PasswordEditForm;
-use app\modules\account\models\PasswordResetForm;
-use app\modules\account\models\SignupForm;
 use Yii;
 use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 class ProfileController extends Controller
 {
     public function behaviors()
     {
-        return [
+        $behaviors = [
             'access' => [
                 'class' => AccessControl::class,
                 'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['login', 'signup', 'password-reset'],
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => ['verify-email'],
-                    ],
                     [
                         'allow' => true,
                         'roles' => ['@'],
                     ],
                 ],
             ],
-//            'verbs' => [
-//                'class' => VerbFilter::className(),
-//                'actions' => [
-//                    'logout' => ['post'],
-//                ],
-//            ],
         ];
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
     }
 
     /**
@@ -55,70 +39,12 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function actionLogin()
-    {
-        $model = new LoginForm(['scenario' => 'login']);
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            /** @var $identity \app\modules\account\models\User */
-            $identity = Yii::$app->user->identity;
-            Yii::$app->session->setFlash('success', Yii::t('app', 'Hello {username}', ['username' => $identity->username]));
-
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    public function actionLogout()
-    {
-        /** @var $identity \app\modules\account\models\User */
-        $identity = Yii::$app->user->identity;
-
-        Yii::$app->user->logout();
-
-        Yii::$app->session->setFlash('success', Yii::t('app', 'Goodbye {username}', ['username' => $identity->username]));
-
-        return $this->goHome();
-    }
-
-    /**
-     * @return string
-     * @throws \yii\base\Exception
-     */
-    public function actionSignup()
-    {
-        $model = new SignupForm(['scenario' => 'signup']);
-
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $model->signup();
-        }
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
-
     /**
      * @return string
      */
     public function actionEdit()
     {
         return $this->render('edit');
-    }
-
-    /**
-     * @return string
-     */
-    public function actionPasswordReset()
-    {
-        $model = new PasswordResetForm(['scenario' => 'password-reset']);
-
-        if ($model->load(Yii::$app->request->get()) && $model->validate()) {
-            $model->reset();
-        }
-        return $this->render('password-reset', [
-            'model' => $model,
-        ]);
     }
 
     /**
@@ -134,16 +60,5 @@ class ProfileController extends Controller
         return $this->render('password-edit', [
             'model' => $model,
         ]);
-    }
-
-    /**
-     * @param $user_id integer
-     * @param $crc string
-     * @return string
-     */
-    public function actionVerifyEmail(/** @noinspection PhpUnusedParameterInspection */
-        $user_id, $crc)
-    {
-        return (string)$user_id . ' ' . (string)$crc;
     }
 }
