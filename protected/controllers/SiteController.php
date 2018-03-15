426b2c6ca9534fe58b881eec93ca0edfcc8faf99
Anthony Lyappiev <admin@antavr.ru>
2018-03-15 15:22:14 +0300

    -

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -17,7 +17,7 @@ class SiteController extends Controller
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+                'fixedVerifyCode' => (YII_ENV_TEST or YII_ENV_DEV) ? 'testme' : null,
             ],
         ];
     }
