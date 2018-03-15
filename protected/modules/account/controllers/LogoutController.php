7c7bec74d4c782fafe5c0467f46fc8677a4a4619
Anthony Lyappiev <admin@antavr.ru>
2018-03-15 14:34:47 +0300

    -

---
+++ protected/modules/account/controllers/LogoutController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\modules\account\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+use yii\web\Controller;
+
+class LogoutController extends Controller
+{
+    public function behaviors()
+    {
+        $behaviors = [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+//            'verbs' => [
+//                'class' => VerbFilter::className(),
+//                'actions' => [
+//                    'index' => ['post'],
+//                ],
+//            ],
+        ];
+
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        /** @var $identity \app\modules\account\models\User */
+        $identity = Yii::$app->user->identity;
+
+        Yii::$app->user->logout();
+
+        Yii::$app->session->setFlash('success', Yii::t('app', 'Goodbye {username}', ['username' => $identity->username]));
+
+        return $this->goHome();
+    }
+}
