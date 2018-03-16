d77675123154664fbab5849c6145c1d68cab4b71
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:08:59 +0300

    -

--- protected/modules/account/views/profile/password-edit.php
+++ protected/modules/account/views/profile/password-edit.php
@@ -16,27 +16,24 @@ $this->params['breadcrumbs'][] = ['label' => $identity->username, 'url' => ['/ac
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<div class="account-profile-password-edit">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-            <?= $form->field($model, 'oldPassword')->passwordInput() ?>
+        <?= $form->field($model, 'oldPassword')->passwordInput() ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
+        <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
         </div>
-    </div>
 
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
