6773ce71d75a73b02b8ae27ebc4c899fbfe8f0f6
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 14:27:43 +0300

    -

---
+++ protected/modules/account/views/password/new.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this \app\components\View */
+
+/* @var $model \app\modules\account\models\PasswordResetForm */
+
+use app\widgets\Captcha;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = Yii::t('app', 'Password Reset');
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="account-profile-password-reset">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php $form = ActiveForm::begin(['id' => 'password-reset-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+
+            <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+
+</div>
