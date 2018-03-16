c76faa08ae499745012b03fc7db474e542252662
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 21:05:34 +0300

    -

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -15,24 +15,20 @@ $this->params['breadcrumbs'][] = $this->title;
 <p><?= Yii::t('app', 'If you have business inquiries or other questions, please fill out the following form to contact us.') ?>
     <?= Yii::t('app', 'Thank you.') ?></p>
 
-<div class="row">
-    <div class="col-lg-6">
-        <?php $form = ActiveForm::begin(); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<?php $form = ActiveForm::begin(['options' => ['class' => 'col-lg-6']]); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+<?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
 
-        <?= $form->field($model, 'email') ?>
+<?= $form->field($model, 'email') ?>
 
-        <?= $form->field($model, 'subject') ?>
+<?= $form->field($model, 'subject') ?>
 
-        <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+<?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
-        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+<?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-    </div>
+<div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
 </div>
+
+<?php ActiveForm::end(); ?>
