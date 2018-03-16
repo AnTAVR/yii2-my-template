85348a2340e307d448ef535cc421680aca15ed58
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:55:48 +0300

    -

--- protected/modules/uploader/views/admin-files/update.php
+++ protected/modules/uploader/views/admin-files/update.php
@@ -11,39 +11,36 @@ $this->title = Yii::t('app', 'Update File: {name}', ['name' => $model->file]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Uploader Files'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
-<div class="file-update">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="file-form">
 
-    <div class="file-form">
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'file',
+            'fileUrl:ntext',
+        ],
+    ]) ?>
 
-        <?= DetailView::widget([
-            'model' => $model,
-            'attributes' => [
-                'id',
-                'file',
-                'fileUrl:ntext',
-            ],
-        ]) ?>
+    <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+    <div class="form-group">
 
-        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+        <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
+
         <div class="form-group">
-
-            <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
-
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn btn-primary']) ?>
-                <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-                    'class' => 'btn btn-danger',
-                    'data' => [
-                        'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                        'method' => 'post',
-                    ],
-                ]) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-
+            <?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn btn-primary']) ?>
+            <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                    'method' => 'post',
+                ],
+            ]) ?>
         </div>
 
+        <?php ActiveForm::end(); ?>
+
     </div>
+</div>
