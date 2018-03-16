0002d3db5c5801fb19f1a2988e2c565be842ad47
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:47:44 +0300

    -

--- protected/views/admin-static/update.php
+++ protected/views/admin-static/update.php
@@ -9,12 +9,8 @@ $this->title = Yii::t('app', 'Update Static Page: {name}', ['name' => $model->co
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Static Pages'), 'url' => ['/admin-static']];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
-<div class="static-page-update">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
-</div>
+<?= $this->render('_form', [
+    'model' => $model,
+]) ?>
