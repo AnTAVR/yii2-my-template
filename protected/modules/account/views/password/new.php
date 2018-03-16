5b27eb77a4d7cafcddf898643f35edf17fffe882
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:09:49 +0300

    -

--- protected/modules/account/views/password/new.php
+++ protected/modules/account/views/password/new.php
@@ -9,27 +9,23 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Password Reset');
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
-<div class="account-profile-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
+        <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
         </div>
-    </div>
 
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
