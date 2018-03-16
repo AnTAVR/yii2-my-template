f1c2c7a12af6254fe3704d5739674568c0952b92
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:59:20 +0300

    -

--- protected/modules/uploader/views/admin-files/create.php
+++ protected/modules/uploader/views/admin-files/create.php
@@ -11,41 +11,37 @@ $this->title = Yii::t('app', 'Create File');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Uploader Files'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="file-create">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="uploader-form">
 
-    <div class="uploader-form">
+    <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form', 'options' => ['enctype' => 'multipart/form-data']]); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form', 'options' => ['enctype' => 'multipart/form-data']]); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+    <?= $form->field($model, 'comment')->textarea(['rows' => 6, 'autofocus' => true]) ?>
 
-        <?= $form->field($model, 'comment')->textarea(['rows' => 6, 'autofocus' => true]) ?>
+    <?= $form->field($model, 'fileUpload')->widget(FileUploadUI::class, [
+        'url' => ['upload'],
+        'formView' => '@app/modules/uploader/views/form',
+        'gallery' => false,
+        'fieldOptions' => [
+            'multiple' => false,
+            'accept' => '*',
+        ],
+        'clientOptions' => [
+            'maxFileSize' => $model::MAX_SIZE,
+        ],
+        'clientEvents' => [
+            'fileuploaddone' => 'function(e, data) {
+                                console.log(e);
+                                console.log(data);
+                            }',
+            'fileuploadfail' => 'function(e, data) {
+                                console.log(e);
+                                console.log(data);
+                            }',
+        ],
+    ]) ?>
 
-        <?= $form->field($model, 'fileUpload')->widget(FileUploadUI::class, [
-            'url' => ['upload'],
-            'formView' => '@app/modules/uploader/views/form',
-            'gallery' => false,
-            'fieldOptions' => [
-                'multiple' => false,
-                'accept' => '*',
-            ],
-            'clientOptions' => [
-                'maxFileSize' => $model::MAX_SIZE,
-            ],
-            'clientEvents' => [
-                'fileuploaddone' => 'function(e, data) {
-                                    console.log(e);
-                                    console.log(data);
-                                }',
-                'fileuploadfail' => 'function(e, data) {
-                                    console.log(e);
-                                    console.log(data);
-                                }',
-            ],
-        ]) ?>
-
-        <?php ActiveForm::end(); ?>
-
-    </div>
+    <?php ActiveForm::end(); ?>
 
 </div>
