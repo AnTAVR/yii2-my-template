7b55c7220cc6023bcc552f1c10ce437036aca119
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 20:52:00 +0300

    -

--- protected/modules/account/views/password/new.php
+++ protected/modules/account/views/password/new.php
@@ -12,20 +12,16 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1><?= Html::encode($this->title) ?></h1>
 
-<div class="row">
-    <div class="col-lg-6">
-        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<?php $form = ActiveForm::begin(['options' => ['class' => 'col-lg-6']]); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+<?= $form->field($model, 'password')->passwordInput() ?>
 
-        <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
+<?= $form->field($model, 'repeatPassword')->passwordInput() ?>
 
-        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+<?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-    </div>
+<div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
 </div>
+
+<?php ActiveForm::end(); ?>
