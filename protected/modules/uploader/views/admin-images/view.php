e3277ded5ea7f60feebac0592ef3bc77aa3c77cc
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:50:12 +0300

    -

--- protected/modules/uploader/views/admin-images/view.php
+++ protected/modules/uploader/views/admin-images/view.php
@@ -10,29 +10,26 @@ $this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Uploader Images'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="image-view">
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'meta_url',
-            'imageUrl',
-            'thumbnailUrl',
-            'thumbnailUrl:image:',
-            'imageUrl:image:',
-            'file',
-            'comment:ntext',
+<p>
+    <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+    <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+        'class' => 'btn btn-danger',
+        'data' => [
+            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+            'method' => 'post',
         ],
     ]) ?>
+</p>
 
-</div>
+<?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+        'meta_url',
+        'imageUrl',
+        'thumbnailUrl',
+        'thumbnailUrl:image:',
+        'imageUrl:image:',
+        'file',
+        'comment:ntext',
+    ],
+]) ?>
