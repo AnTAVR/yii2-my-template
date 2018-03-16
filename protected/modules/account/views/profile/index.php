126599a29766fdcd6a15fc581b2122a5cbd9071b
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:09:09 +0300

    -

--- protected/modules/account/views/profile/index.php
+++ protected/modules/account/views/profile/index.php
@@ -10,23 +10,21 @@ $this->title = $identity->username;
 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="account-profile-index">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4><?= Yii::t('app', 'Profile') ?></h4>
-        </div>
-        <div class="panel-body">
-            <?= DetailView::widget([
-                'model' => $identity,
-                'attributes' => [
-                    'id',
-                    'username',
-                    'foto',
-                ],
-            ]) ?>
-        </div>
-        <div class="panel-footer">
-            <?= Html::a(Yii::t('app', 'Password Edit'), ['password-edit'], ['class' => 'btn btn-default']) ?>
-        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h4><?= Yii::t('app', 'Profile') ?></h4>
+    </div>
+    <div class="panel-body">
+        <?= DetailView::widget([
+            'model' => $identity,
+            'attributes' => [
+                'id',
+                'username',
+                'foto',
+            ],
+        ]) ?>
+    </div>
+    <div class="panel-footer">
+        <?= Html::a(Yii::t('app', 'Password Edit'), ['password-edit'], ['class' => 'btn btn-default']) ?>
     </div>
 </div>
