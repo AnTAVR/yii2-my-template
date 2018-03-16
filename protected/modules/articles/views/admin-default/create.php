12cf014ad825793d58fddec154a45591e4b5b7d4
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:11:19 +0300

    -

--- protected/modules/articles/views/admin-default/create.php
+++ protected/modules/articles/views/admin-default/create.php
@@ -10,12 +10,8 @@ $this->title = Yii::t('app', 'Create Articles');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Articles'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="articles-create">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
-</div>
+<?= $this->render('_form', [
+    'model' => $model,
+]) ?>
