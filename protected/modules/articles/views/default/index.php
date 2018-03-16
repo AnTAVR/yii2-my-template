d9ec1e3a1a44a93898842d46f9f090881328df11
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:04:50 +0300

    -

--- protected/modules/articles/views/default/index.php
+++ protected/modules/articles/views/default/index.php
@@ -10,29 +10,26 @@ use yii\widgets\LinkPager;
 $this->title = Yii::t('app', 'Articles');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="articles-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php $i = 0;
-    $col = 1;
-    foreach ($data as $model) {
-        /* @var $model \app\modules\articles\models\Articles */
-        if (!$i) {
-            echo '<div class="row">', "\n";
-            $open = true;
-        }
-        echo $this->render('_index', ['model' => $model, 'class' => 'col-sm-6']);
-        if ($i >= $col) {
-            echo '</div>', "\n\n";
-            $i = 0;
-        } else {
-            $i++;
-        }
+<h1><?= Html::encode($this->title) ?></h1>
+<?php $i = 0;
+$col = 1;
+foreach ($data as $model) {
+    /* @var $model \app\modules\articles\models\Articles */
+    if (!$i) {
+        echo '<div class="row">', "\n";
+        $open = true;
     }
-    if ($i) {
+    echo $this->render('_index', ['model' => $model, 'class' => 'col-sm-6']);
+    if ($i >= $col) {
         echo '</div>', "\n\n";
+        $i = 0;
+    } else {
+        $i++;
     }
-    ?>
+}
+if ($i) {
+    echo '</div>', "\n\n";
+}
+?>
 
-    <div class="row"><?= LinkPager::widget(['pagination' => $pagination,]) ?></div>
-</div>
+<div class="row"><?= LinkPager::widget(['pagination' => $pagination,]) ?></div>
