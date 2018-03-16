51769858ca2a832ba406fe745bc59b3af2da63c9
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:09:40 +0300

    -

--- protected/modules/account/views/profile/edit.php
+++ protected/modules/account/views/profile/edit.php
@@ -10,15 +10,13 @@ $this->params['breadcrumbs'][] = $this->title;
 /* @var $context yii\web\Controller */
 $context = $this->context;
 ?>
-<div class="account-profile-edit">
-    <h1><?= $context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $context->action->id ?>".
-        The action belongs to the controller "<?= get_class($context) ?>"
-        in the "<?= $context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+<h1><?= $context->action->uniqueId ?></h1>
+<p>
+    This is the view content for action "<?= $context->action->id ?>".
+    The action belongs to the controller "<?= get_class($context) ?>"
+    in the "<?= $context->module->id ?>" module.
+</p>
+<p>
+    You may customize this page by editing the following file:<br>
+    <code><?= __FILE__ ?></code>
+</p>
