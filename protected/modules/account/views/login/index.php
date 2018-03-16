bd6ce616944f184c865c02b397df47fb5a583d4b
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:10:15 +0300

    -

--- protected/modules/account/views/login/index.php
+++ protected/modules/account/views/login/index.php
@@ -10,36 +10,34 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Login');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="account-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <p><?= Yii::t('app', 'Please fill out the following fields to login:') ?></p>
+<p><?= Yii::t('app', 'Please fill out the following fields to login:') ?></p>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-            <?php
-            /** @var \app\modules\account\Module $module */
-            $module = Yii::$app->controller->module;
-            if ($module->params['signup']) {
-                echo $form->field($model, 'username')->textInput(['autofocus' => true])->hint(Html::a(Yii::t('app', 'Signup'), ['/account/signup'], ['class' => 'label label-success']));
-            } else {
-                echo $form->field($model, 'username')->textInput(['autofocus' => true]);
-            }
-            ?>
+        <?php
+        /** @var \app\modules\account\Module $module */
+        $module = Yii::$app->controller->module;
+        if ($module->params['signup']) {
+            echo $form->field($model, 'username')->textInput(['autofocus' => true])->hint(Html::a(Yii::t('app', 'Signup'), ['/account/signup'], ['class' => 'label label-success']));
+        } else {
+            echo $form->field($model, 'username')->textInput(['autofocus' => true]);
+        }
+        ?>
 
-            <?= $form->field($model, 'password')->passwordInput([''])->hint(Html::a(Yii::t('app', 'Password Reset'), ['/account/password'], ['class' => 'label label-danger'])) ?>
+        <?= $form->field($model, 'password')->passwordInput([''])->hint(Html::a(Yii::t('app', 'Password Reset'), ['/account/password'], ['class' => 'label label-danger'])) ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
