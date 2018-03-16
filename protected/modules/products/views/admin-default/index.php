58d29f2ee73765121a76f59d7f0cd7efeb61b608
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:01:51 +0300

    -

--- protected/modules/products/views/admin-default/index.php
+++ protected/modules/products/views/admin-default/index.php
@@ -9,41 +9,38 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Products');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="products-index">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Products'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= /** @noinspection PhpUnusedParameterInspection */
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            'id',
-            'productsUrl',
-            'content_title',
-            'published_at',
-            'status_txt',
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{viewP} {view} {update} {delete}',
-                'buttons' => [
-                    'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
-                        $url, $model, $key) {
-                        /* @var $model \app\modules\products\models\Products */
-                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
-                            $model->productsUrl,
-                            [
-                                'title' => Yii::t('app', 'View site'),
-                                'aria-label' => Yii::t('app', 'View site'),
-                                'data-pjax' => '0',
-                                'target' => '_blank',
-                                'style' => 'color: red;',
-                            ]);
-                    },
-                ],
+<p>
+    <?= Html::a(Yii::t('app', 'Create Products'), ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+<?= /** @noinspection PhpUnusedParameterInspection */
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'id',
+        'productsUrl',
+        'content_title',
+        'published_at',
+        'status_txt',
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'template' => '{viewP} {view} {update} {delete}',
+            'buttons' => [
+                'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
+                    $url, $model, $key) {
+                    /* @var $model \app\modules\products\models\Products */
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
+                        $model->productsUrl,
+                        [
+                            'title' => Yii::t('app', 'View site'),
+                            'aria-label' => Yii::t('app', 'View site'),
+                            'data-pjax' => '0',
+                            'target' => '_blank',
+                            'style' => 'color: red;',
+                        ]);
+                },
             ],
         ],
-    ]); ?>
-</div>
+    ],
+]); ?>
