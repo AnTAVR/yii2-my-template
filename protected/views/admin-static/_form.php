360a610d18b65edd0f2b74434ea4896c64e3e215
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 20:15:08 +0300

    -

--- protected/views/admin-static/_form.php
+++ protected/views/admin-static/_form.php
@@ -8,28 +8,23 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 ?>
+<?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-<div class="static-page-form">
+<?= $form->field($model, 'meta_url')->textInput(['maxlength' => true]) ?>
 
-    <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<?= $form->field($model, 'content_title')->textInput(['maxlength' => true, 'autofocus' => true]) ?>
 
-    <?= $form->field($model, 'meta_url')->textInput(['maxlength' => true]) ?>
+<?= $form->field($model, 'content_full')->widget(CKEditor::class, [
+    'options' => ['rows' => 6],
+    'preset' => 'full'
+]) ?>
 
-    <?= $form->field($model, 'content_title')->textInput(['maxlength' => true, 'autofocus' => true]) ?>
+<?= $form->field($model, 'meta_description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content_full')->widget(CKEditor::class, [
-        'options' => ['rows' => 6],
-        'preset' => 'full'
-    ]) ?>
+<?= $form->field($model, 'meta_keywords')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'meta_description')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'meta_keywords')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+<div class="form-group">
+    <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
 </div>
+
+<?php ActiveForm::end(); ?>
