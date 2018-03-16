215d2e05806e5d908f6eb51330c1cf01d8773ba7
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:52:16 +0300

    -

--- protected/modules/uploader/views/admin-images/index.php
+++ protected/modules/uploader/views/admin-images/index.php
@@ -9,41 +9,38 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Uploader Images');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="image-index">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<p>
+    <?= Html::a(Yii::t('app', 'Create Image'), ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+<?= /** @noinspection PhpUnusedParameterInspection */
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'id',
+        'thumbnailUrl',
+        'thumbnailUrl:image:',
+        'comment:ntext',
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Image'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= /** @noinspection PhpUnusedParameterInspection */
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            'id',
-            'thumbnailUrl',
-            'thumbnailUrl:image:',
-            'comment:ntext',
-
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{viewP} {view} {update} {delete}',
-                'buttons' => [
-                    'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
-                        $url, $model, $key) {
-                        /* @var $model \app\modules\uploader\models\UploaderImage */
-                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
-                            $model->imageUrl,
-                            [
-                                'title' => Yii::t('app', 'View site'),
-                                'aria-label' => Yii::t('app', 'View site'),
-                                'data-pjax' => '0',
-                                'target' => '_blank',
-                                'style' => 'color: red;',
-                            ]);
-                    },
-                ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'template' => '{viewP} {view} {update} {delete}',
+            'buttons' => [
+                'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
+                    $url, $model, $key) {
+                    /* @var $model \app\modules\uploader\models\UploaderImage */
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
+                        $model->imageUrl,
+                        [
+                            'title' => Yii::t('app', 'View site'),
+                            'aria-label' => Yii::t('app', 'View site'),
+                            'data-pjax' => '0',
+                            'target' => '_blank',
+                            'style' => 'color: red;',
+                        ]);
+                },
             ],
         ],
-    ]); ?>
-</div>
+    ],
+]); ?>
