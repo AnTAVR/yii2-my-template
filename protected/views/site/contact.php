8f3cb425153b550d51c856513948f0801f46840c
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 16:45:40 +0300

    -

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -10,32 +10,29 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Feedback');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <p><?= Yii::t('app', 'If you have business inquiries or other questions, please fill out the following form to contact us.') ?>
-        <?= Yii::t('app', 'Thank you.') ?></p>
+<p><?= Yii::t('app', 'If you have business inquiries or other questions, please fill out the following form to contact us.') ?>
+    <?= Yii::t('app', 'Thank you.') ?></p>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'email') ?>
+        <?= $form->field($model, 'email') ?>
 
-            <?= $form->field($model, 'subject') ?>
+        <?= $form->field($model, 'subject') ?>
 
-            <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+        <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Send'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
         </div>
-    </div>
 
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
