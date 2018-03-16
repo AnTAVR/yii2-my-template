7bc5b82bf6aa826211414bf46e7cd7ff35a795ca
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:05:22 +0300

    -

--- protected/modules/news/views/admin-default/index.php
+++ protected/modules/news/views/admin-default/index.php
@@ -9,41 +9,38 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'News');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="news-index">
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create News'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= /** @noinspection PhpUnusedParameterInspection */
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            'id',
-            'newsUrl',
-            'content_title',
-            'published_at',
-            'status_txt',
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{viewP} {view} {update} {delete}',
-                'buttons' => [
-                    'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
-                        $url, $model, $key) {
-                        /* @var $model \app\modules\news\models\News */
-                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
-                            $model->newsUrl,
-                            [
-                                'title' => Yii::t('app', 'View site'),
-                                'aria-label' => Yii::t('app', 'View site'),
-                                'data-pjax' => '0',
-                                'target' => '_blank',
-                                'style' => 'color: red;',
-                            ]);
-                    },
-                ],
+<p>
+    <?= Html::a(Yii::t('app', 'Create News'), ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+<?= /** @noinspection PhpUnusedParameterInspection */
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'id',
+        'newsUrl',
+        'content_title',
+        'published_at',
+        'status_txt',
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'template' => '{viewP} {view} {update} {delete}',
+            'buttons' => [
+                'viewP' => function (/** @noinspection PhpUnusedParameterInspection */
+                    $url, $model, $key) {
+                    /* @var $model \app\modules\news\models\News */
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>',
+                        $model->newsUrl,
+                        [
+                            'title' => Yii::t('app', 'View site'),
+                            'aria-label' => Yii::t('app', 'View site'),
+                            'data-pjax' => '0',
+                            'target' => '_blank',
+                            'style' => 'color: red;',
+                        ]);
+                },
             ],
         ],
-    ]); ?>
-</div>
+    ],
+]); ?>
