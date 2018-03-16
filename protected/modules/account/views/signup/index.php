213d8bc669f1ce4edad9e0ae088d1474e921619e
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 17:08:46 +0300

    -

--- protected/modules/account/views/signup/index.php
+++ protected/modules/account/views/signup/index.php
@@ -10,31 +10,28 @@ $this->title = Yii::t('app', 'Signup');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<div class="<?= $this->divClass ?>">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin(['id' => $this->uniqueId . '-form']); /* @var $form \yii\bootstrap\ActiveForm */ ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'email')->textInput() ?>
+        <?= $form->field($model, 'email')->textInput() ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
+        <?= $form->field($model, 'repeatPassword')->passwordInput() ?>
 
-            <?= $form->field($model, 'verifyRules')->checkbox()->hint(Html::a(Yii::t('app', 'Rules'), ['/static/index', 'meta_url' => 'rules'], ['class' => 'label label-success', 'target' => '_blank'])) ?>
+        <?= $form->field($model, 'verifyRules')->checkbox()->hint(Html::a(Yii::t('app', 'Rules'), ['/static/index', 'meta_url' => 'rules'], ['class' => 'label label-success', 'target' => '_blank'])) ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+        <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
         </div>
-    </div>
 
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
