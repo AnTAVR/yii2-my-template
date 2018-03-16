f8b98dcdfcbe99b783b43d3944fe3fae07a9e427
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 12:25:29 +0300

    -

--- protected/modules/uploader/Module.php
+++ protected/modules/uploader/Module.php
@@ -20,6 +20,7 @@ class Module extends ModuleOld
     public function init()
     {
         parent::init();
+        $this->params = require __DIR__ . '/config/params.php';
 
         // custom initialization code goes here
     }
