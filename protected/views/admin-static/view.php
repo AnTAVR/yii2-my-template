4750c62821f153c36308a64b8d6be6cb8b4ec3da
Anthony Lyappiev <admin@antavr.ru>
2018-03-16 20:07:05 +0300

    -

--- protected/views/admin-static/view.php
+++ protected/views/admin-static/view.php
@@ -21,28 +21,25 @@ if (!empty($model->meta_keywords)) {
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Static Pages'), 'url' => ['/admin-static']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="static-page-view static-page-<?= $model->meta_url ?>">
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'meta_url',
-            'staticUrl',
-            'content_title',
-            'content_full:raw',
-            'meta_description',
-            'meta_keywords',
+<p>
+    <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+    <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+        'class' => 'btn btn-danger',
+        'data' => [
+            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+            'method' => 'post',
         ],
     ]) ?>
+</p>
 
-</div>
+<?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+        'meta_url',
+        'staticUrl',
+        'content_title',
+        'content_full:raw',
+        'meta_description',
+        'meta_keywords',
+    ],
+]) ?>
