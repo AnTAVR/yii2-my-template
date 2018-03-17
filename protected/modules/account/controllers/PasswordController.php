ea666c8a7f19e4b68aaa76bed6851ea407ec92df
Anthony Lyappiev <admin@antavr.ru>
2018-03-17 19:54:37 +0300

    -

--- protected/modules/account/controllers/PasswordController.php
+++ protected/modules/account/controllers/PasswordController.php
@@ -4,7 +4,10 @@ namespace app\modules\account\controllers;
 
 use app\modules\account\models\PasswordForm;
 use app\modules\account\models\PasswordNewForm;
+use app\modules\account\models\Token;
+use app\modules\account\models\User;
 use Yii;
+use yii\helpers\Url;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
@@ -13,6 +16,7 @@ class PasswordController extends Controller
     /**
      * Renders the index view for the module
      * @return string
+     * @throws \yii\base\Exception
      */
     public function actionIndex()
     {
@@ -23,7 +27,29 @@ class PasswordController extends Controller
         $model = new PasswordForm();
 
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail()) {
+
+            $user = User::findOne(['email' => $this->email]);
+
+            $security = Yii::$app->security;
+            $tokenModel = new Token([
+                'user_id' => $user->id,
+                'code' => $security->generateRandomString(),
+                'type' => Token::TYPE_CONFIRM_EMAIL,
+                'expires_on' => time() + 60 * 60 * 24,
+            ]);
+            $tokenModel->save();
+
+            $url = Url::to(['new', 'token' => $tokenModel->code], true);
+            $body = Yii::t('app', 'To password recovery, follow the link: {url}', ['url' => $url]);
+            $subject = Yii::t('app', 'Password recovery from {site}', ['site' => Yii::$app->name]);
+
+            $ret = Yii::$app->mailer->compose()
+                ->setTo($user->email)
+                ->setFrom([Yii::$app->params['supportEmail'] => Yii::t('app', '{appname} robot', ['appname' => Yii::$app->name])])
+                ->setSubject($subject)
+                ->setTextBody($body)
+                ->send();
+            if ($ret) {
                 Yii::$app->session->addFlash('success', Yii::t('app', 'A letter with instructions was sent to E-Mail.'));
             } else {
                 Yii::$app->session->addFlash('error', Yii::t('app', 'There was an error sending email.'));
@@ -35,30 +61,33 @@ class PasswordController extends Controller
         ]);
     }
 
+    /** @noinspection PhpUndefinedClassInspection */
     /**
-     * @param $user_id integer
      * @param $token string
      * @return string
      * @throws NotFoundHttpException
+     * @throws \Exception
      * @throws \yii\base\Exception
+     * @throws \yii\db\StaleObjectException
+     * @throws \Throwable
      */
-    public function actionNew($user_id, $token)
+    public function actionNew($token)
     {
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
 
-        $model = PasswordNewForm::findOne($user_id);
-        if (!$model) {
-            throw new NotFoundHttpException(Yii::t('app', 'User not found.'));
-        }
+        $tokenModel = Token::findByCode($token, Token::TYPE_CONFIRM_EMAIL);
 
-        if (!$model->validatePasswordToken($token)) {
-            throw new NotFoundHttpException(Yii::t('app', 'User not found.'));
-        }
+        $model = PasswordNewForm::findOne($tokenModel->user_id);
 
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $model->edit();
+            $security = Yii::$app->security;
+            $model->password_hash = $security->generatePasswordHash($model->password);
+            $model->save(false);
+
+            $tokenModel->delete();
+
             Yii::$app->session->addFlash('success', Yii::t('app', 'New password was saved.'));
             return $this->redirect(Yii::$app->user->loginUrl);
         }
