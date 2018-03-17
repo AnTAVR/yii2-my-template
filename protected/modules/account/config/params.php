88383504d1492780c0c7c03073384cc4d7d7e888
Anthony Lyappiev <admin@antavr.ru>
2018-03-17 21:07:40 +0300

    Перенес параметры

--- protected/modules/account/config/params.php
+++ protected/modules/account/config/params.php
@@ -5,4 +5,10 @@ return [
     'adminPageSize' => 100,
     'expires_confirm_email' => 60 * 60 * 24,
     'expires_recovery_password' => 60 * 60 * 24,
+    'username.max' => 16,
+    'username.min' => 2,
+    'username.pattern' => '/^[A-Za-z1-9\-_]+$/s',
+    'username.hint' => 'A-Z a-z 0-9 -_',
+    'password.min' => 6,
+    'password.max' => 128,
 ];
