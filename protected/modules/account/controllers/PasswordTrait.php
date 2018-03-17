f2aa1e60c2ea58953401adf32a46b06bc481222d
Anthony Lyappiev <admin@antavr.ru>
2018-03-17 23:11:42 +0300

    -

---
+++ protected/modules/account/controllers/PasswordTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @property string $tokenRaw
+ * @property string $token
+ * @property string $salt
+ * @property string $password_hash
+ */
+trait PasswordTrait
+{
+    public function getTokenRaw()
+    {
+        return $this->salt . $this->password_hash;
+    }
+
+    public function getToken()
+    {
+        return hash('sha256', $this->tokenRaw);
+    }
+
+}
