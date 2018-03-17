1a3c340062e76b8db879866b08dc5ccdf62f5c87
Anthony Lyappiev <admin@antavr.ru>
2018-03-17 20:55:07 +0300

    -

--- protected/modules/account/config/params.php
+++ protected/modules/account/config/params.php
@@ -1,6 +1,8 @@
 <?php
 return [
-    'duration' => 3600 * 24 * 30,
+    'duration' => 60 * 60 * 24 * 30,
     'signup' => true, // Разрешить регистрацию на сайте.
     'adminPageSize' => 100,
+    'expires_confirm_email' => 60 * 60 * 24,
+    'expires_recovery_password' => 60 * 60 * 24,
 ];
