b1d88c4f984ec1694a12b0ed957dab67750b0e61
Anthony Lyappiev <admin@antavr.ru>
2018-03-18 15:49:40 +0300

    -

---
+++ protected/modules/account/actions/LoginAction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\modules\account\actions;
+
+use app\modules\account\models\LoginForm;
+use app\modules\account\models\User;
+use Yii;
+use yii\base\Action;
+use yii\db\Expression;
+
+class LoginAction extends Action
+{
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->controller->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            $user = User::findOne(['username' => $model->username]);
+            if ($user->status == User::STATUS_ACTIVE) {
+                $params = require __DIR__ . '/../config/params.php';
+
+                $login = Yii::$app->user->login($user, $model->rememberMe ? $params['duration'] : 0);
+                if ($login) {
+                    $user->session = Yii::$app->session->id;
+                    $user->session_at = new Expression('NOW()');
+                    $user->save();
+
+                    return $this->controller->goBack();
+                }
+            } else {
+                $model->addError('username', Yii::t('app', 'User "{username}" status: "{status}"', ['username' => $user->username, 'status' => $user->getStatusName()]));
+            }
+        }
+        return $this->controller->render('@app/modules/account/views/login/index', [
+            'model' => $model,
+        ]);
+    }
+
+}
--- protected/modules/account/controllers/LoginController.php
+++ protected/modules/account/controllers/LoginController.php
@@ -2,42 +2,17 @@
 
 namespace app\modules\account\controllers;
 
-use app\modules\account\models\LoginForm;
-use app\modules\account\models\User;
-use Yii;
-use yii\db\Expression;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 class LoginController extends Controller
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
-    public function actionIndex()
+    public function actions()
     {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+        $actions = [
+            'index' => 'app\modules\account\actions\LoginAction',
+        ];
 
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $user = User::findOne(['username' => $model->username]);
-            if ($user->status == User::STATUS_ACTIVE) {
-                $login = Yii::$app->user->login($user, $model->rememberMe ? $this->module->params['duration'] : 0);
-                if ($login) {
-                    $user->session = Yii::$app->session->id;
-                    $user->session_at = new Expression('NOW()');
-                    $user->save();
-
-                    return $this->goBack();
-                }
-            } else {
-                $model->addError('username', Yii::t('app', 'User "{username}" status: "{status}"', ['username' => $user->username, 'status' => $user->getStatusName()]));
-            }
-        }
-        return $this->render('index', [
-            'model' => $model,
-        ]);
+        return ArrayHelper::merge(parent::actions(), $actions);
     }
 }
