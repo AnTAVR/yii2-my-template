a6df78bbb1bb9505060ace3f694eb7315280a417
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 17:22:48 +0300

    -

--- protected/config/common.php
+++ protected/config/common.php
@@ -3,10 +3,10 @@
 use yii\helpers\ArrayHelper;
 
 $params = require __DIR__ . '/common/params.php';
-$db = require __DIR__ . '/db.php';
+$db = require __DIR__ . '/common/db.php';
 
 /** @noinspection HtmlUnknownTag */
-$config = [
+$__config = [
     'language' => $params['language'],
     'name' => $params['appName'],
     'id' => 'common',
@@ -113,19 +113,19 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
+    $__config['bootstrap'][] = 'gii';
+    $__config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
+    $__config['bootstrap'][] = 'debug';
+    $__config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
-return $config;
+return $__config;
