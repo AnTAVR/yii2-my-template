174fb9e4447436a85186be2eeb4407a30871a4a6
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 16:39:03 +0300

    -

--- protected/config/console.php
+++ protected/config/console.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\ArrayHelper;
 
-$__config = [
+return ArrayHelper::merge(require __DIR__ . '/common.php', [
     'id' => 'basic-console',
     'controllerNamespace' => 'app\commands',
     'controllerMap' => [
@@ -13,17 +13,4 @@ $__config = [
             ],
         */
     ],
-];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $__config['bootstrap'][] = 'gii';
-
-    $__config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
-}
-
-return ArrayHelper::merge(require __DIR__ . '/common.php', $__config);
+]);
