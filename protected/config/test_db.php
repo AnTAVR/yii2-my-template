2519f7362741fb136de01e4fcb72a1c8aa88f553
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 16:20:28 +0300

    -

--- protected/config/test_db.php
+++ protected/config/test_db.php
@@ -2,8 +2,6 @@
 
 use yii\helpers\ArrayHelper;
 
-$_db_tmp = [
+return ArrayHelper::merge(require __DIR__ . '/db.php', [
     'dsn' => 'mysql:host=localhost;dbname=tests_tests',
-];
-
-return ArrayHelper::merge(require __DIR__ . '/db.php', $_db_tmp);
+]);
