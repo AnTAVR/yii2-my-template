747e8306d005b35280196d0067bbb7cc7d79d69c
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 15:15:58 +0300

    -

--- protected/components/View.php
+++ protected/components/View.php
@@ -2,19 +2,8 @@
 
 namespace app\components;
 
-use Yii;
 use yii\web\View as OldView;
 
 class View extends OldView
 {
-    public $uniqueId;
-    public $divClass;
-
-    public function beforeRender($viewFile, $params)
-    {
-        $controller = Yii::$app->controller;
-        $this->uniqueId = str_replace('/', '-', $controller->action->uniqueId);
-        $this->divClass = str_replace('/', '-', $controller->uniqueId);
-        return parent::beforeRender($viewFile, $params);
-    }
 }
