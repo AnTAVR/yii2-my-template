cdb37d2f81185fbc89bc018190d075a293604cb0
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 13:48:21 +0300

    -

--- protected/components/Formatter.php
+++ protected/components/Formatter.php
@@ -2,16 +2,8 @@
 
 namespace app\components;
 
-use yii\helpers\ArrayHelper;
 use yii\i18n\Formatter as OldFormatter;
 
 class Formatter extends OldFormatter
 {
-    public function behaviors()
-    {
-        $behaviors = [
-
-        ];
-        return ArrayHelper::merge(parent::behaviors(), $behaviors);
-    }
 }
