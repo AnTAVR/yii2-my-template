75578dc37e9381c2f7fbd1f792218206fe2ba717
Anthony Lyappiev <admin@antavr.ru>
2018-03-19 15:37:24 +0300

    -

--- protected/config/container.php
+++ protected/config/container.php
@@ -3,6 +3,10 @@ return [
     'definitions' => [
         'yii\i18n\Formatter' => 'app\components\Formatter',
         'yii\web\View' => 'app\components\View',
+        'yii\web\Session' => [
+            'class' => 'app\components\Session',
+            'savePath' => '@runtime/session',
+        ],
     ],
     'singletons' => [
     ],
