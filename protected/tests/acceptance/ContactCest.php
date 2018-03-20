efa6262a2a9f43166f6c06ddeb617b1d23fd1d5d
Anthony Lyappiev <admin@antavr.ru>
2018-03-20 20:12:23 +0300

    -

--- protected/tests/acceptance/ContactCest.php
+++ protected/tests/acceptance/ContactCest.php
@@ -11,7 +11,6 @@ class ContactCest
 
     public function contactPageWorks(\AcceptanceTester $I)
     {
-        $I->wantTo('ensure that contact page works');
         $I->see('Contact', 'h1');
     }
 
