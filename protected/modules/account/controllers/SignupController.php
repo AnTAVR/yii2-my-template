c5262e05a89f08d1c90c7f5a6d6235d1006c1ac4
Anthony Lyappiev <admin@antavr.ru>
2018-03-20 18:36:19 +0300

    -

--- protected/modules/account/controllers/SignupController.php
+++ protected/modules/account/controllers/SignupController.php
@@ -31,44 +31,8 @@ class SignupController extends Controller
 
         $model = new SignupForm();
 
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $security = Yii::$app->security;
-
-            $model->password_hash = $security->generatePasswordHash($model->password);
-            $model->salt = $security->generateRandomString(64);
-            $model->auth_key = $security->generateRandomString();
-            $model->created_ip = Yii::$app->request->isConsoleRequest ? '(console)' : Yii::$app->request->userIP;
-            $model->email_confirmed = (int)false;
-
-            if ($model->save(false)) {
-                //the following three lines were added:
-//                $auth = Yii::$app->authManager;
-//                $authorRole = $auth->getRole('author');
-//                $auth->assign($authorRole, $model->getId());
-
-                Yii::$app->session->addFlash('success', Yii::t('app', 'Account successfully registered.'));
-
-                $tokenModel = new UserToken([
-                    'user_id' => $model->id,
-                    'code' => $model->tokenEmail,
-                    'type' => UserToken::TYPE_CONFIRM_EMAIL,
-                    'expires_on' => time() + $this->module->params['expires_confirm_email'],
-                ]);
-
-                if ($tokenModel->save()) {
-                    if ($model->sendEmail_VerifyEmail($tokenModel)) {
-                        Yii::$app->session->addFlash('success', Yii::t('app', 'A letter with instructions was sent to E-Mail.'));
-                    } else {
-                        Yii::$app->session->addFlash('error', Yii::t('app', 'There was an error sending email.'));
-                    }
-                    return $this->goHome();
-                } else {
-                    $txt = Yii::t('app', 'A letter with instructions has already been sent to E-Mail.');
-                    Yii::$app->session->addFlash('error', $txt);
-                    $model->addError('email', $txt);
-                }
-                return $this->goHome();
-            }
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            return $this->goHome();
         }
         return $this->render('index', [
             'model' => $model,
