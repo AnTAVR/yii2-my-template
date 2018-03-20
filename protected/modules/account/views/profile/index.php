82979d65200c17279e51d31a30b2cdd2d0105707
Anthony Lyappiev <admin@antavr.ru>
2018-03-20 15:17:08 +0300

    -

--- protected/modules/account/views/profile/index.php
+++ protected/modules/account/views/profile/index.php
@@ -35,7 +35,12 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </div>
     <div class="panel-footer">
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete'], ['data' => ['method' => 'post'], 'class' => 'btn btn-danger']) ?>
+        <?= Html::a(Yii::t('app', 'Delete'), ['delete'], ['class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete account?'),
+                'method' => 'post',
+            ],
+        ]) ?>
         <?= Html::a(Yii::t('app', 'Password Edit'), ['password-edit'], ['class' => 'btn btn-default']) ?>
     </div>
 </div>
