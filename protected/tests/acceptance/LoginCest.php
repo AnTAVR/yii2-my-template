0cf5b305589f8d681e240ec2d11852cb675fa042
Anthony Lyappiev <admin@antavr.ru>
2018-03-20 20:07:21 +0300

    -

--- protected/tests/acceptance/LoginCest.php
+++ protected/tests/acceptance/LoginCest.php
@@ -4,14 +4,21 @@ use yii\helpers\Url;
 
 class LoginCest
 {
-    public function ensureThatLoginWorks(\AcceptanceTester $I)
+    public function _before(\AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/account/login'));
-        $I->see('Login', 'h1');
+    }
 
+    public function homePageWorks(\AcceptanceTester $I)
+    {
+        $I->see('Login', 'h1');
+    }
+
+    public function ensureThatLoginWorks(\AcceptanceTester $I)
+    {
         $I->amGoingTo('try to login with correct credentials');
         $I->fillField('input[name="LoginForm[username]"]', 'admin');
-        $I->fillField('input[name="LoginForm[password]"]', 'admin');
+        $I->fillField('input[name="LoginForm[password]"]', 'adminadmin');
         $I->click('login-button');
         $I->wait(2); // wait for button to be clicked
 
