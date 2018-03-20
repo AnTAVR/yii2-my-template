a2263262a10a70e3332b85c006e232dd005023b9
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 02:52:49 +0300

    -

--- protected/tests/_support/FunctionalTester.php
+++ protected/tests/_support/FunctionalTester.php
@@ -1,35 +1,18 @@
 <?php
 
-use \Codeception\Actor;
+use Codeception\Actor;
 
-
-/**
- * Inherited Methods
- * @method void wantToTest($text)
- * @method void wantTo($text)
- * @method void execute($callable)
- * @method void expectTo($prediction)
- * @method void expect($prediction)
- * @method void amGoingTo($argumentation)
- * @method void see($string, $string1 = NULL)
- * @method void dontSeeElement($string)
- * @method void dontSee($string, $string1)
- * @method void amOnPage($string)
- * @method void amOnRoute($string)
- * @method void amLoggedInAs($string)
- * @method void am($role)
- * @method void seeEmailIsSent()
- * @method void submitForm($string, $array)
- * @method void lookForwardTo($achieveValue)
- * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
- *
- * @SuppressWarnings(PHPMD)
- */
 class FunctionalTester extends Actor
 {
-    use/** @noinspection PhpUndefinedNamespaceInspection */
-        /** @noinspection PhpUndefinedClassInspection */
-        _generated\FunctionalTesterActions;
+    use _generated\FunctionalTesterActions;
 
+    public function seeValidationError($message)
+    {
+        $this->see($message, '.help-block');
+    }
+
+    public function dontSeeValidationError($message)
+    {
+        $this->dontSee($message, '.help-block');
+    }
 }
