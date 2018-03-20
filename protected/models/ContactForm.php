de103a7cf86aa9cbc4769362735e476602940823
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 02:07:12 +0300

    -

--- protected/models/ContactForm.php
+++ protected/models/ContactForm.php
@@ -68,7 +68,7 @@ class ContactForm extends Model
      * @param string $email the target email address
      * @return bool whether the model passes validation
      */
-    public function contact($email)
+    public function sendEmail($email)
     {
         return Yii::$app->mailer->compose()
             ->setTo($email)
