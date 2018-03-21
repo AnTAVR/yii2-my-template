7dac8542be656540ea257287f259fa75d71311c1
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 14:43:56 +0300

    -

--- protected/run-tests.sh
+++ protected/run-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./yii serve -t '@webroot' &
-PHP_PID=${!}
+#./yii serve -t '@webroot' &
+#PHP_PID=${!}
 
 #./tests/bin/yii migrate --interactive=0
 #./vendor/bin/codecept -vvv build
 cd ..
 ./protected/vendor/bin/codecept -vvv run -c ./protected
 
-kill ${PHP_PID}
+#kill ${PHP_PID}
