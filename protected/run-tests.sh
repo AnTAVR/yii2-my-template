586a520945e100ae84a1bbbabdc4be81f1bf18e4
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 15:47:12 +0300

    -

--- protected/run-tests.sh
+++ protected/run-tests.sh
@@ -6,6 +6,6 @@
 #./tests/bin/yii migrate --interactive=0
 #./vendor/bin/codecept -vvv build
 cd ..
-./protected/vendor/bin/codecept -vvv run -c ./protected
+./protected/vendor/bin/codecept run -c ./protected
 
 #kill ${PHP_PID}
