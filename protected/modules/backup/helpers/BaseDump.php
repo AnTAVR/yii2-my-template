35fe81cc308515ea6b1b07359df5c0296103714b
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 18:31:13 +0300

    -

--- protected/modules/backup/helpers/BaseDump.php
+++ protected/modules/backup/helpers/BaseDump.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\helpers;
+namespace app\modules\backup\helpers;
 
 abstract class BaseDump
 {
--- protected/modules/backup/helpers/PostgresDump.php
+++ protected/modules/backup/helpers/PostgresDump.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\helpers;
+namespace app\modules\backup\helpers;
 
 use yii\helpers\StringHelper;
 
