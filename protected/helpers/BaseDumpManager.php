bac65c377b506277cf07bd56810cc72023e88db4
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 17:53:13 +0300

    -

---
+++ protected/helpers/BaseDumpManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\helpers;
+
+abstract class BaseDumpManager
+{
+    /**
+     * @param string $path
+     * @param string $dbName
+     * @return string
+     */
+    public static function makePath($path, $dbName)
+    {
+        return sprintf('%s%s_%s.%s',
+            $path,
+            $dbName,
+            date('Y-m-d_H-i-s'),
+            'sql.gz'
+        );
+    }
+
+    /**
+     * @param string $path
+     * @param string $dbName
+     * @param string $host
+     * @param string $username
+     * @param string $password
+     * @param string $port
+     * @return string
+     */
+    abstract public static function makeDumpCommand($path, $dbName, $host, $username, $password, $port);
+
+    /**
+     * @param string $path
+     * @param string $dbName
+     * @param string $host
+     * @param string $username
+     * @param string $password
+     * @param string $port
+     * @return string
+     */
+    abstract public static function makeRestoreCommand($path, $dbName, $host, $username, $password, $port);
+
+    /**
+     * @return bool
+     */
+    public static function isWindows()
+    {
+        return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+    }
+}
