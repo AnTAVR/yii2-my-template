6ad9578c8d441c1233c7f5ca731cc6a5f1d21aa3
Anthony Lyappiev <admin@antavr.ru>
2018-03-21 03:08:27 +0300

    -

--- protected/tests/unit/_bootstrap.php
+++ protected/tests/unit/_bootstrap.php
@@ -1,3 +1,16 @@
 <?php
-
-// add unit testing specific bootstrap code here
+/**
+ * Here you can initialize variables via \Codeception\Util\Fixtures class
+ * to store data in global array and use it in Tests.
+ *
+ * ```php
+ * // Here _bootstrap.php
+ * \Codeception\Util\Fixtures::add('user1', ['name' => 'davert']);
+ * ```
+ *
+ * In Tests
+ *
+ * ```php
+ * \Codeception\Util\Fixtures::get('user1');
+ * ```
+ */
