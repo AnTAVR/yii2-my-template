ed3f639e3847edd72f8e3365c668790581f14765
Anthony Lyappiev <admin@antavr.ru>
2018-03-23 17:30:02 +0300

    -

--- protected/views/admin-dump/index.php
+++ protected/views/admin-dump/index.php
@@ -3,7 +3,11 @@
 use yii\helpers\Html;
 
 /* @var $this \app\components\View */
+/* @var $dataProvider \yii\data\ArrayDataProvider */
+/* @var $activePids array */
 
 $this->title = Yii::t('app', 'Dump');
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <h1><?= Html::encode($this->title) ?></h1>
