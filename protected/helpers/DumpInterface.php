f5b40d255d0e1b1d6b39d98391c5515c6e3d87c3
Anthony Lyappiev <admin@antavr.ru>
2018-03-23 15:43:04 +0300

    -

---
+++ protected/helpers/DumpInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\helpers;
+
+abstract class DumpInterface
+{
+
+    /**
+     * @param string $dumpFile
+     * @param array $dbInfo
+     * @return string
+     */
+    abstract public static function makeDumpCommand($dumpFile, $dbInfo);
+
+    /**
+     * @param string $dumpFile
+     * @param array $dbInfo
+     * @return string
+     */
+    abstract public static function makeRestoreCommand($dumpFile, $dbInfo);
+
+    /**
+     * @return bool
+     */
+    public static function isWindows()
+    {
+        return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+    }
+}
