fa0641251bc439da15a4e2dc3d6a103dfab39c1e
Anthony Lyappiev <admin@antavr.ru>
2018-03-23 16:58:12 +0300

    -

--- protected/controllers/AdminDumpController.php
+++ protected/controllers/AdminDumpController.php
@@ -3,16 +3,30 @@
 namespace app\controllers;
 
 use app\components\AdminController;
+use yii\helpers\ArrayHelper;
 
 /**
  * AdminDumpController.
  */
 class AdminDumpController extends AdminController
 {
-    /**
-     * Lists all StaticPage models.
-     * @return mixed
-     */
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'verbs' => [
+                'class' => '\yii\filters\VerbFilter',
+                'actions' => [
+                    'create' => ['post'],
+                    'delete' => ['post'],
+                    'delete-all' => ['post'],
+                    'restore' => ['get', 'post'],
+                    '*' => ['get'],
+                ],
+            ],
+        ]);
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
