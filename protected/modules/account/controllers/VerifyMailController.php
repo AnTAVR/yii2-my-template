1651da568628ba4bd08755c9b0399661c3462dd4
Anthony Lyappiev <admin@antavr.ru>
2018-03-24 16:22:26 +0300

    Переместил проверку email в отдельный контроллер.

---
+++ protected/modules/account/controllers/VerifyMailController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\modules\account\controllers;
+
+use app\modules\account\models\SignupForm;
+use app\modules\account\models\User;
+use app\modules\account\models\UserToken;
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class VerifyMailController extends Controller
+{
+    /** @noinspection PhpUndefinedClassInspection */
+    /**
+     * @param $token string
+     * @return string
+     * @throws NotFoundHttpException
+     * @throws \Exception
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
+     */
+    public function actionIndex($token)
+    {
+        $tokenModel = UserToken::findByCode($token, UserToken::TYPE_CONFIRM_EMAIL);
+
+        $model = SignupForm::findOne($tokenModel->user_id);
+        $tokenModel->delete();
+
+        if ($model->status == User::STATUS_ACTIVE) {
+            if ($model->tokenEmail !== $tokenModel->code) {
+                throw new NotFoundHttpException(Yii::t('app', 'Token not found!'));
+            }
+
+            $model->email_confirmed = true;
+            $model->save(false);
+            Yii::$app->session->addFlash('success', Yii::t('app', 'E-Mail is verified, now you can login.'));
+        } else {
+            $txt = Yii::t('app', 'User status: "{status}"', ['status' => $model->getStatusName()]);
+            Yii::$app->session->addFlash('error', $txt);
+            $model->addError('user', $txt);
+        }
+
+        return $this->redirect(Yii::$app->user->loginUrl);
+    }
+}
