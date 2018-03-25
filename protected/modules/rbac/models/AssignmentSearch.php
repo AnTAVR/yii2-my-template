d46f6418564ed6f0024c86c5be7a5f8b7fc30015
Anthony Lyappiev <admin@antavr.ru>
2018-03-25 20:54:14 +0300

    -

---
+++ protected/modules/rbac/models/AssignmentSearch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\modules\rbac\models;
+
+use app\modules\account\models\User;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+class AssignmentSearch extends Model
+{
+    /**
+     *
+     * @var mixed $id
+     */
+    public $id;
+
+    /**
+     *
+     * @var string $login
+     */
+    public $login;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['id', 'safe'],
+            ['login', 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'login' => Yii::t('app', 'Username'),
+        ];
+    }
+
+    /**
+     * Create data provider for Assignment model.
+     */
+    public function search()
+    {
+        $query = User::find();
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+        $params = Yii::$app->request->getQueryParams();
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere(['id' => $this->id]);
+        $query->andFilterWhere(['like', 'username', $this->login]);
+
+        return $dataProvider;
+    }
+
+}
