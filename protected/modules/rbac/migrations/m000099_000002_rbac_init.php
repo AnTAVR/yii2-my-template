452df8c26fbb314a0fec144567dd0ce11372ec8f
Anthony Lyappiev <admin@antavr.ru>
2018-03-26 12:46:49 +0300

    -

---
+++ protected/modules/rbac/migrations/m000099_000002_rbac_init.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @noinspection PhpIncludeInspection */
+require_once Yii::getAlias('@vendor') . '/yiisoft/yii2/rbac/migrations/m140506_102106_rbac_init.php';
+
+class m000099_000002_rbac_init extends m140506_102106_rbac_init
+{
+}
---
+++ protected/modules/rbac/migrations/m000099_000003_rbac_add_index_on_auth_assignment_user_id.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @noinspection PhpIncludeInspection */
+require_once Yii::getAlias('@vendor') . '/yiisoft/yii2/rbac/migrations/m170907_052038_rbac_add_index_on_auth_assignment_user_id.php';
+
+class m000099_000003_rbac_add_index_on_auth_assignment_user_id extends m170907_052038_rbac_add_index_on_auth_assignment_user_id
+{
+}
