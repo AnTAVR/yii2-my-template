66e048711130c01d60e73474439f5c157cebfdce
Anthony Lyappiev <admin@antavr.ru>
2018-03-26 14:50:40 +0300

    -

--- protected/widgets/Captcha.php
+++ protected/widgets/Captcha.php
@@ -7,7 +7,7 @@ use yii\captcha\Captcha as CaptchaOld;
 
 class Captcha extends CaptchaOld
 {
-    public $options = ['value' => '', 'class' => 'form-control'];
+    public $options = ['value' => YII_ENV_TEST || YII_ENV_DEV ? 'testme' : '', 'class' => 'form-control'];
     public $template = '<div class="row"><div class="col-lg-4">{image}</div><div class="col-lg-6">{input}</div></div>';
     public $imageOptions = ['style' => 'cursor: pointer;'];
     public $captchaAction = '/site/captcha';
