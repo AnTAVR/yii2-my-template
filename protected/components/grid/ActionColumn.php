b44fe02e2eb83abc72645a590e6aca0d284ceb5f
Anthony Lyappiev <admin@antavr.ru>
2018-03-27 17:56:13 +0300

    -

--- protected/components/grid/ActionColumn.php
+++ protected/components/grid/ActionColumn.php
@@ -4,9 +4,25 @@ namespace app\components\grid;
 
 use Yii;
 use yii\grid\ActionColumn as OldActionColumn;
+use yii\helpers\Url;
 
 class ActionColumn extends OldActionColumn
 {
+    public function init()
+    {
+        parent::init();
+        $this->urlCreator = function (/** @noinspection PhpUnusedParameterInspection */
+            $action, $model, $key, $index, $this_is) {
+            if ($action === 'viewP') {
+                return $model->url;
+            }
+            $params = is_array($key) ? $key : ['id' => (string)$key];
+            $params[0] = $this->controller ? $this->controller . '/' . $action : $action;
+
+            return Url::toRoute($params);
+        };
+    }
+
     protected function initDefaultButtons()
     {
         $this->initDefaultButton('viewP', 'eye-open', [
