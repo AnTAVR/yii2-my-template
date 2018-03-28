82c26392aed0d31da3c78ddc3dd993cb992cc82c
Anthony Lyappiev <admin@antavr.ru>
2018-03-28 19:12:12 +0300

    -

--- protected/views/static/index.php
+++ protected/views/static/index.php
@@ -4,19 +4,19 @@
 /* @var $model \app\models\StaticPage */
 /* @var $breadcrumbs boolean */
 
-$this->title = $model->content_title;
 if (!empty($model->meta_description))
 {
     $this->registerMetaTag([
         'name' => 'description',
         'content' => $model->meta_description]);
 }
-
 if (!empty($model->meta_keywords)) {
     $this->registerMetaTag([
         'name' => 'keywords',
         'content' => $model->meta_keywords]);
 }
+
+$this->title = $model->content_title;
 if (!isset($breadcrumbs)) {
     $this->params['breadcrumbs'][] = $this->title;
 }
