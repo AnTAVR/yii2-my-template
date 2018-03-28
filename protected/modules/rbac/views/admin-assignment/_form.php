50a1efc4d4d23765b23033425f159eee63cfeb3e
Anthony Lyappiev <admin@antavr.ru>
2018-03-28 14:04:29 +0300

    поправил

---
+++ protected/modules/rbac/views/admin-assignment/_form.php
@@ -0,0 +1,30 @@
+<?php
+
+/* @var $this \yii\web\View */
+
+/* @var $model \app\modules\rbac\models\forms\AssignmentForm */
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$authManager = Yii::$app->authManager;
+
+$roles = ArrayHelper::map($authManager->getRoles(), 'name', 'name');
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+<?= $form->field($model, 'userId')->textInput() ?>
+
+<?= $form->field($model, 'roles')->checkboxList($roles, [
+    'separator' => '<br>',
+//    'item' => fu,
+]) ?>
+
+<div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Save'),
+        [
+            'class' => 'btn btn-primary',
+        ]) ?>
+</div>
+<?php ActiveForm::end(); ?>
--- protected/modules/rbac/views/admin-assignment/update.php
+++ protected/modules/rbac/views/admin-assignment/update.php
@@ -4,31 +4,13 @@
 
 /* @var $model \app\modules\rbac\models\forms\AssignmentForm */
 
-use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 
 $this->title = Yii::t('app', 'User Assignment');
 $this->params['breadcrumbs'][] = $this->title;
-
-$authManager = Yii::$app->authManager;
-
-$roles = ArrayHelper::map($authManager->getRoles(), 'name', 'name');
-
 ?>
-<?php $form = ActiveForm::begin(); ?>
-<?= $form->field($model, 'userId')->textInput() ?>
+<h1><?= Html::encode($this->title) ?></h1>
 
-<?= $form->field($model, 'roles')->checkboxList($roles, [
-    'separator' => '<br>',
-//    'item' => fu,
+<?= $this->render('_form', [
+    'model' => $model,
 ]) ?>
-
-<div class="form-group">
-    <?= Html::submitButton(Yii::t('app', 'Save'),
-        [
-            'class' => 'btn btn-primary',
-        ]) ?>
-</div>
-<?php ActiveForm::end(); ?>
-
