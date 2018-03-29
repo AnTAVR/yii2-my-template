9d93f1a4be8528c43dfe44439fa9293da3342e69
Anthony Lyappiev <admin@antavr.ru>
2018-03-30 01:59:19 +0300

    -

--- protected/modules/rbac/views/admin-permission/update.php
+++ protected/modules/rbac/views/admin-permission/update.php
@@ -1,8 +1,20 @@
 <?php
 
-/* @var $this \yii\web\View */
-/* @var $model \app\modules\rbac\models\Permission */
+use yii\helpers\Html;
+
+/**
+ * @var $this \yii\web\View
+ * @var $model \app\modules\rbac\models\Permission
+ */
+
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Permissions Manager'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->name]];
+
+$this->title = Yii::t('yii', 'Update');
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<h1><?= Html::encode($this->title) ?></h1>
+
 <?= $this->render('_form', [
     'model' => $model,
 ]) ?>
