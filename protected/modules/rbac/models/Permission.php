92d3c454bda9b43fdc2e9f6b6a12ebf286400bf3
Anthony Lyappiev <admin@antavr.ru>
2018-03-29 23:08:10 +0300

    -

--- protected/modules/rbac/models/Permission.php
+++ protected/modules/rbac/models/Permission.php
@@ -2,20 +2,9 @@
 
 namespace app\modules\rbac\models;
 
-use Yii;
 use yii\rbac\Item;
 
-/**
- * Description of Permission
- */
 class Permission extends AuthItem
 {
     public $type = Item::TYPE_PERMISSION;
-
-    public static function find($name)
-    {
-        $authManager = Yii::$app->authManager;
-        $item = $authManager->getPermission($name);
-        return new self($item);
-    }
 }
