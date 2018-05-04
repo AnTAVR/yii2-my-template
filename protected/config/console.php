337c91f7ea2360d06987a283a3965d88e0a058b5
Anthony Lyappiev <admin@antavr.ru>
2018-05-04 22:28:49 +0300

    -

--- protected/config/console.php
+++ protected/config/console.php
@@ -6,11 +6,11 @@ return ArrayHelper::merge(require __DIR__ . '/common.php', [
     'id' => 'basic-console',
     'controllerNamespace' => 'app\commands',
     'controllerMap' => [
-        'migrate' => 'bariew\moduleMigration\ModuleMigrateController',
-        /*
-            'fixture' => [ // Fixture generation command line.
-                'class' => 'yii\faker\FixtureController',
-            ],
-        */
+        'migrate' => [
+            'class' => 'bariew\moduleMigration\ModuleMigrateController',
+        ],
+        'fixture' => [
+            'class' => 'yii\faker\FixtureController',
+        ],
     ],
 ]);
