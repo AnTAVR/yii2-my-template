5081c8e93151536776327d3101e155d62dac3122
Anthony Lyappiev <admin@antavr.ru>
2018-05-07 00:30:24 +0300

    -

---
+++ protected/widgets/LinkPager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\LinkPager as oldLinkPager;
+
+class LinkPager extends oldLinkPager
+{
+    protected function renderPageButtons()
+    {
+        $pageCount = $this->pagination->getPageCount();
+        if ($pageCount < 2 && $this->hideOnSinglePage) {
+            return '';
+        }
+
+        $buttons = [];
+        $currentPage = $this->pagination->getPage();
+
+        // first page
+        $firstPageLabel = $this->firstPageLabel === true ? '1' : $this->firstPageLabel;
+        if ($firstPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($firstPageLabel, 0, $this->firstPageCssClass, $currentPage <= 0, false);
+        }
+
+        // prev page
+        if ($this->prevPageLabel !== false) {
+            if (($page = $currentPage - 1) < 0) {
+                $page = 0;
+            }
+            $buttons[] = $this->renderPageButton($this->prevPageLabel, $page, $this->prevPageCssClass, $currentPage <= 0, false);
+        }
+
+        // internal pages
+        list($beginPage, $endPage) = $this->getPageRange();
+        for ($i = $beginPage; $i <= $endPage; ++$i) {
+            $buttons[] = $this->renderPageButton($i + 1, $i, null, $this->disableCurrentPageButton && $i == $currentPage, $i == $currentPage);
+        }
+
+        // next page
+        if ($this->nextPageLabel !== false) {
+            if (($page = $currentPage + 1) >= $pageCount - 1) {
+                $page = $pageCount - 1;
+            }
+            $buttons[] = $this->renderPageButton($this->nextPageLabel, $page, $this->nextPageCssClass, $currentPage >= $pageCount - 1, false);
+        }
+
+        // last page
+        $lastPageLabel = $this->lastPageLabel === true ? $pageCount : $this->lastPageLabel;
+        if ($lastPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($lastPageLabel, $pageCount - 1, $this->lastPageCssClass, $currentPage >= $pageCount - 1, false);
+        }
+
+        $buttons[] = $this->renderJumpPage();
+
+        $options = $this->options;
+        $tag = ArrayHelper::remove($options, 'tag', 'ul');
+        return Html::tag($tag, implode("\n", $buttons), $options);
+    }
+
+    protected function renderJumpPage()
+    {
+        return null;
+    }
+}
