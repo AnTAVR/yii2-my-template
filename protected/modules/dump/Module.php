6719bdb4dabee4704b6efaf3b1fba7f9ea6f690e
Anthony Lyappiev <admin@antavr.ru>
2018-05-06 15:43:09 +0300

    -

---
+++ protected/modules/dump/Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\modules\dump;
+
+use app\components\Module as ModuleOld;
+
+class Module extends ModuleOld
+{
+//    public function init()
+//    {
+//        parent::init();
+//
+//        $this->params = require __DIR__ . '/config/params.php';
+//
+//        // custom initialization code goes here
+//    }
+}
