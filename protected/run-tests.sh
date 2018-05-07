96a6e97c5f7d077b23ccada3af1e52ba6edcd14c
Anthony Lyappiev <admin@antavr.ru>
2018-05-07 22:11:04 +0300

    -

--- protected/run-tests.sh
+++ protected/run-tests.sh
@@ -5,9 +5,9 @@
 
 #./yii-test migrate --interactive=0
 #./yii-test migrate --migrationPath=@yii/rbac/migrations --interactive=0
-./vendor/bin/codecept -vvv build
+./vendor/bin/codecept build
 #cd ..
 #./protected/vendor/bin/codecept run -c ./protected --coverage-html
-./vendor/bin/codecept run --coverage-html
+./vendor/bin/codecept run --coverage-html -vv
 
 #kill ${PHP_PID}
