b14540321809f3a21e4917b2d572be0ca1f79807
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 16:14:46 +0300

    -

--- protected/modules/account/migrations/m000099_000000_insert_user.php
+++ protected/modules/account/migrations/m000099_000000_insert_user.php
@@ -3,9 +3,6 @@
 use app\modules\account\models\User;
 use yii\db\Migration;
 
-/**
- * Handles the creation for table `user`.
- */
 class m000099_000000_insert_user extends Migration
 {
     public $tableName;
@@ -32,23 +29,10 @@ class m000099_000000_insert_user extends Migration
             'salt' => $security->generateRandomString(64),
             'password_hash' => $security->generatePasswordHash('adminadmin')
         ]);
-
-
-        $this->insert($this->tableName, [
-            'username' => 'tests',
-            'created_at' => time(),
-            'email' => 'tests@tests.tests',
-            'email_confirmed' => true,
-            'status' => User::STATUS_ACTIVE,
-            'auth_key' => $security->generateRandomString(),
-            'salt' => $security->generateRandomString(64),
-            'password_hash' => Yii::$app->security->generatePasswordHash('adminadmin')
-        ]);
     }
 
     public function down()
     {
-        $this->delete($this->tableName, ['username' => 'tests']);
         $this->delete($this->tableName, ['username' => 'admin']);
     }
 
