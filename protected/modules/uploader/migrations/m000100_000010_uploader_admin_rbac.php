10e078e8f5ff532dbbe03bf40e7c29029324f12c
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 15:38:59 +0300

    -

--- protected/modules/uploader/migrations/m000100_000010_uploader_admin_rbac.php
+++ protected/modules/uploader/migrations/m000100_000010_uploader_admin_rbac.php
@@ -2,7 +2,7 @@
 
 use app\modules\account\components\AdminPanelMigration;
 
-class m000100_000010_uploader_rbac extends AdminPanelMigration
+class m000100_000010_uploader_admin_rbac extends AdminPanelMigration
 {
     const PERMISSION_ADMIN = 'uploader.openAdminPanel';
 }
