da66b1334a50e1c5184cf4581e4c7ad9dd09cf51
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 15:31:15 +0300

    -

--- protected/modules/news/migrations/m000103_000010_news_rbac.php
+++ protected/modules/news/migrations/m000103_000010_news_rbac.php
@@ -1,26 +1,8 @@
 <?php
 
-use yii\db\Migration;
+use app\modules\account\components\AdminPanelMigration;
 
-class m000103_000010_news_rbac extends Migration
+class m000103_000010_news_rbac extends AdminPanelMigration
 {
-    public function up()
-    {
-        $authManager = Yii::$app->authManager;
-
-        $permission = $authManager->createPermission('news.openAdminPanel');
-        $permission->description = 'Open News Admin Panel';
-        $authManager->add($permission);
-
-        $authManager->addChild($permission, $authManager->getPermission('site.openAdminPanel'));
-
-        $authManager->addChild($authManager->getRole('root-role'), $permission);
-    }
-
-    public function down()
-    {
-        $authManager = Yii::$app->authManager;
-
-        $authManager->remove($authManager->getPermission('news.openAdminPanel'));
-    }
+    const PERMISSION_ADMIN = 'news.openAdminPanel';
 }
