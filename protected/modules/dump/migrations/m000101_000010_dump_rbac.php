31f6dfc931bc57cafd6f35a0371b6fecdd58b5aa
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 15:36:41 +0300

    -

--- protected/modules/dump/migrations/m000101_000010_dump_rbac.php
+++ protected/modules/dump/migrations/m000101_000010_dump_rbac.php
@@ -1,26 +1,8 @@
 <?php
 
-use yii\db\Migration;
+use app\modules\account\components\AdminPanelMigration;
 
-class m000101_000010_dump_rbac extends Migration
+class m000101_000010_dump_rbac extends AdminPanelMigration
 {
-    public function up()
-    {
-        $authManager = Yii::$app->authManager;
-
-        $permission = $authManager->createPermission('dump.openAdminPanel');
-        $permission->description = 'Open Dump Admin Panel';
-        $authManager->add($permission);
-
-        $authManager->addChild($permission, $authManager->getPermission('site.openAdminPanel'));
-
-        $authManager->addChild($authManager->getRole('root-role'), $permission);
-    }
-
-    public function down()
-    {
-        $authManager = Yii::$app->authManager;
-
-        $authManager->remove($authManager->getPermission('dump.openAdminPanel'));
-    }
+    const PERMISSION_ADMIN = 'dump.openAdminPanel';
 }
