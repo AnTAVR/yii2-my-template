e8227c687843db3217743218cd293fac9fe79bba
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 15:35:44 +0300

    -

--- protected/modules/statics/migrations/m000101_000011_static_admin_rbac.php
+++ protected/modules/statics/migrations/m000101_000011_static_admin_rbac.php
@@ -2,7 +2,7 @@
 
 use app\modules\account\components\AdminPanelMigration;
 
-class m000101_000011_static_rbac extends AdminPanelMigration
+class m000101_000011_static_admin_rbac extends AdminPanelMigration
 {
     const PERMISSION_ADMIN = 'statics.openAdminPanel';
 }
