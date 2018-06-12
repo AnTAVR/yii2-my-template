aeebcd326ce68e86f945ed2298c2427c7d90a57e
Anthony Lyappiev <admin@antavr.ru>
2018-06-12 15:40:19 +0300

    -

--- protected/modules/rbac/migrations/m000099_000005_rbac_admin_rbac.php
+++ protected/modules/rbac/migrations/m000099_000005_rbac_admin_rbac.php
@@ -2,7 +2,7 @@
 
 use app\modules\account\components\AdminPanelMigration;
 
-class m000099_000005_rbac_rbac extends AdminPanelMigration
+class m000099_000005_rbac_admin_rbac extends AdminPanelMigration
 {
     const PERMISSION_ADMIN = 'rbac.openAdminPanel';
 }
