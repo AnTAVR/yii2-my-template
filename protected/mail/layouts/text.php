ecd9fba3bd16eecc032bbac29723416d850a4066
Anthony Lyappiev <admin@antavr.ru>
2020-07-04 21:35:29 +0300

    -

--- protected/mail/layouts/text.php
+++ protected/mail/layouts/text.php
@@ -1,7 +1,7 @@
 <?php
 
-/* @var $this \yii\web\View view component instance */
-/* @var $message \yii\mail\MessageInterface the message being composed */
+/* @var $this yii\web\View view component instance */
+/* @var $message yii\mail\MessageInterface the message being composed */
 /* @var $content string main view render result */
 ?>
 <?php $this->beginPage() ?>
