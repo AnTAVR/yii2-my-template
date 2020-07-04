900e2e6d0e9dcf1658b2d8210d9997db7a812ab1
Anthony Lyappiev <admin@antavr.ru>
2020-07-04 21:36:27 +0300

    -

--- protected/modules/account/views/profile/password-edit.php
+++ protected/modules/account/views/profile/password-edit.php
@@ -1,7 +1,7 @@
 <?php
-/* @var $this \yii\web\View */
+/* @var $this yii\web\View */
 
-/* @var $model \app\modules\account\models\forms\PasswordEditForm */
+/* @var $model app\modules\account\models\forms\PasswordEditForm */
 
 use app\widgets\Captcha;
 use yii\bootstrap\ActiveForm;
@@ -9,7 +9,7 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Password Edit');
 
-/** @var $identity \app\modules\account\models\User */
+/** @var $identity app\modules\account\models\User */
 $identity = Yii::$app->user->identity;
 $this->params['breadcrumbs'][] = ['label' => $identity->username, 'url' => ['/account']];
 
