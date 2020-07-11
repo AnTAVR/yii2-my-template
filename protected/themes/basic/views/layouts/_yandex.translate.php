0cf16d149f4ec378602a0e08b09190b28f79fb74
Anthony Lyappiev <admin@antavr.ru>
2020-07-11 18:17:41 +0300

    -

---
+++ protected/themes/basic/views/layouts/_yandex.translate.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\web\View;
+
+/* @var $this View */
+?>
+
+<li class="dropdown">
+    <div id="ytWidget" class="dropdown-toggle"></div>
+</li>
+<script src="https://translate.yandex.net/website-widget/v1/widget.js?widgetId=ytWidget&pageLang=ru&widgetTheme=light&autoMode=false"
+        type="text/javascript"></script>
+<style>
+    #yt-widget .yt-servicelink {
+        display: none;
+    }
+
+    #ytWidget {
+        padding-top: 7px;
+    }
+</style>
