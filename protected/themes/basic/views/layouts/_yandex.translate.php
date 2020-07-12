deba2b6071d9156a6a8190e5e45f8df85c62c7c7
Anthony Lyappiev <admin@antavr.ru>
2020-07-12 16:09:48 +0300

    -

--- protected/themes/basic/views/layouts/_yandex.translate.php
+++ protected/themes/basic/views/layouts/_yandex.translate.php
@@ -8,7 +8,7 @@ use yii\web\View;
 <div id="ytWidget"></div>
 <script src="https://translate.yandex.net/website-widget/v1/widget.js?widgetId=ytWidget&pageLang=ru&widgetTheme=light&autoMode=false"
         type="text/javascript"></script>
-<style>
+<style type="text/css">
     #yt-widget .yt-servicelink {
         display: none;
     }
